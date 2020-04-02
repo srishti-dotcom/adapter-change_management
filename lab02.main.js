628897366301d4f1fe8a60bf59345e6c01138291
srishti-dotcom <srishti.tomar93@gmail.com>
2020-04-02 04:20:21 +0000

    Lab 2

--- lab02.main.js
+++ lab02.main.js
@@ -1,6 +1,6 @@
 // Update this constant with your ServiceNow credentials
 const options = {
-  url: 'https://dev82988.service-now.com/',
+  url: 'https://dev82988.service-now.com/navpage.do',
   username: 'admin',
   password: 'Srish@1001'
 };
